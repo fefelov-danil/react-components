eb5f39ff02de36d44a00f8160951ac215cb3acff
Daniil Fefelov <fefelov.danil@gmail.com>
2023-07-29 16:51:12 +0400

    feat: int and textarea

---
+++ src/components/inputPassword/InputPassword.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, DetailedHTMLProps, forwardRef, InputHTMLAttributes, KeyboardEvent, useState } from 'react'
+import style from './InputPassword.module.scss'
+
+type DefaultInputPasswordPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type InputPasswordPropsType = DefaultInputPasswordPropsType & {
+  fieldName?: string
+  onChangePassword?: (value: string) => void
+  onEnter?: () => void
+  error?: string
+  spanClassName?: string
+}
+
+type Ref = HTMLInputElement
+
+export const InputPassword = forwardRef<Ref, InputPasswordPropsType>(
+  (
+    { fieldName, onChange, onChangePassword, onKeyDown, onEnter, error, className, spanClassName, ...restProps },
+    ref
+  ) => {
+    InputPassword.displayName = 'InputText'
+
+    const [showPass, setShowPass] = useState(false)
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange && onChange(e)
+      onChangePassword && onChangePassword(e.currentTarget.value)
+    }
+    const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
+      onKeyDown && onKeyDown(e)
+      onEnter && e.key === 'Enter' && onEnter()
+    }
+
+    const finalSpanClassName = `${style.error} ${spanClassName ? spanClassName : ''}`
+    const finalInputClassName = `${style.inputPassword} ${error && style.errorInput} ${className}`
+
+    return (
+      <>
+        <label className={`${style.inputContainer} ${finalInputClassName}`}>
+          {fieldName && <span className={style.fieldName}>{fieldName}</span>}
+          <input
+            ref={ref}
+            type={showPass ? 'text' : 'password'}
+            onChange={onChangeCallback}
+            onKeyDown={onKeyPressCallback}
+            {...restProps}
+          />
+          <span onClick={() => setShowPass(!showPass)}>{showPass ? '@' : '#'}</span>
+          {error && <span className={finalSpanClassName}>{error}</span>}
+        </label>
+      </>
+    )
+  }
+)
---
+++ src/components/inputText/InputText.tsx
@@ -0,0 +1,43 @@
+import { ChangeEvent, DetailedHTMLProps, forwardRef, InputHTMLAttributes, KeyboardEvent } from 'react'
+import style from './InputText.module.scss'
+
+type DefaultInputTextPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type InputTextPropsType = DefaultInputTextPropsType & {
+  fieldName?: string
+  onChangeText?: (value: string) => void
+  onEnter?: () => void
+  error?: string
+  spanClassName?: string
+}
+
+type Ref = HTMLInputElement
+
+export const InputText = forwardRef<Ref, InputTextPropsType>(
+  ({ fieldName, onChange, onChangeText, onKeyDown, onEnter, error, className, spanClassName, ...restProps }, ref) => {
+    InputText.displayName = 'InputText'
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      onChange && onChange(e)
+      onChangeText && onChangeText(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+      onKeyDown && onKeyDown(e)
+      onEnter && e.key === 'Enter' && onEnter()
+    }
+
+    const finalSpanClassName = `${style.error} ${spanClassName ? spanClassName : ''}`
+    const finalInputClassName = `${style.inputText} ${error && style.errorInput} ${className}`
+
+    return (
+      <>
+        <label className={`${style.inputContainer} ${finalInputClassName}`}>
+          {fieldName && <span className={style.fieldName}>{fieldName}</span>}
+          <input ref={ref} type={'text'} onChange={onChangeHandler} onKeyDown={onKeyPressHandler} {...restProps} />
+          {error && <span className={finalSpanClassName}>{error}</span>}
+        </label>
+      </>
+    )
+  }
+)
